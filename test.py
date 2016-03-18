3d97efa2978b93699c3496043eed6b00e3d3a6f5
mitchvogel <mjv58@cornell.edu>
2016-03-18 15:06:10 -0500

    have an ok classifier

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import numpy as np
 import cv2
  
 cascade = cv2.CascadeClassifier('classifier/cascade.xml')
-img = cv2.imread('test/t3.jpg')
+img = cv2.imread('test/l1.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-bulbs = cascade.detectMultiScale(gray, 2, 5)
+bulbs = cascade.detectMultiScale(gray, 1.3, 5)
 for(x,y,w,h) in bulbs:
 	cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
